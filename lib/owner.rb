d43ca10e75c4e434a4eb3781ba22af32c935f20d
Julie  McManus <julieannesays@gmail.com>
2020-02-08 00:39:15 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,10 +1,11 @@
 class Owner
-  attr_accessor :name
+  attr_accessor :name, :species
 
 @@owners = []
 
   def initialize(name)
     @name = name
+    @species = "human"
     @@owners << self
   end
 
